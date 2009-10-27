31f1dcb2ff8b272845ce7c61cf2b3ec6a48ca3e1
luzifer42 <luzifer42@a247ce00-be0d-11de-ac5a-05e7513655e2>
2009-10-27 11:16:52 +0000

    Added MemorySeach window

---
+++ trunk/DSexplorer/src/luz/dsexplorer/gui/MemorySearch.java
@@ -0,0 +1,164 @@
+package luz.dsexplorer.gui;
+
+import java.awt.Dimension;
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+
+import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+
+public class MemorySearch extends javax.swing.JPanel {
+	private JTextField txtSearch;
+	private JComboBox cbType;
+	private JLabel jLabel2;
+	private JButton btnAdd;
+	private JComboBox cbValue;
+	private JLabel jLabel1;
+	private JLabel lblValue;
+	private JScrollPane jScrollPane1;
+	private JTable tblResults;
+	private JButton btnNext;
+	private JButton btnFirst;
+
+	/**
+	* Auto-generated main method to display this 
+	* JPanel inside a new JFrame.
+	*/
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new MemorySearch());
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public MemorySearch() {
+		super();
+		initGUI();
+	}
+	
+	private void initGUI() {
+		try {
+			GroupLayout thisLayout = new GroupLayout((JComponent)this);
+			this.setLayout(thisLayout);
+			setPreferredSize(new Dimension(400, 300));
+			{
+				txtSearch = new JTextField();
+			}
+			{
+				btnFirst = new JButton();
+				btnFirst.setText("First Scan");
+				btnFirst.setMargin(new java.awt.Insets(2, 2, 2, 2));
+			}
+			{
+				ComboBoxModel cbTypeModel = 
+					new DefaultComboBoxModel(
+							new String[] { "Exact Search", "Pattern Search" });
+				cbType = new JComboBox();
+				cbType.setModel(cbTypeModel);
+			}
+			{
+				btnNext = new JButton();
+				btnNext.setText("Next Scan");
+				btnNext.setMargin(new java.awt.Insets(2, 2, 2, 2));
+			}
+			{
+				jScrollPane1 = new JScrollPane();
+				{
+					TableModel tblResultsModel = 
+						new DefaultTableModel(
+								new String[][] { { "0x0001", "1" }, { "0x0002", "2" } },
+								new String[] { "Address", "Value" });
+					tblResults = new JTable();
+					jScrollPane1.setViewportView(tblResults);
+					tblResults.setModel(tblResultsModel);
+				}
+			}
+			{
+				btnAdd = new JButton();
+				btnAdd.setText("<- Add");
+			}
+			{
+				lblValue = new JLabel();
+				lblValue.setText("Value");
+			}
+			{
+				jLabel1 = new JLabel();
+				jLabel1.setText("Scan Tye");
+			}
+			{
+				ComboBoxModel cbValueModel = 
+					new DefaultComboBoxModel(
+							new String[] { "2 Bytes", "4 Bytes" });
+				cbValue = new JComboBox();
+				cbValue.setModel(cbValueModel);
+			}
+			{
+				jLabel2 = new JLabel();
+				jLabel2.setText("Value Type");
+			}
+			thisLayout.setVerticalGroup(thisLayout.createSequentialGroup()
+				.addContainerGap()
+				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(btnNext, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(btnFirst, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(txtSearch, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(lblValue, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addGroup(thisLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				    .addComponent(cbType, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				    .addComponent(jLabel1, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addGroup(thisLayout.createParallelGroup()
+				    .addComponent(cbValue, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+				        .addGap(8)
+				        .addComponent(jLabel2, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addComponent(jScrollPane1, 0, 121, Short.MAX_VALUE)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				.addComponent(btnAdd, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+				.addContainerGap());
+			thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
+				.addContainerGap()
+				.addGroup(thisLayout.createParallelGroup()
+				    .addGroup(thisLayout.createSequentialGroup()
+				        .addGroup(thisLayout.createParallelGroup()
+				            .addComponent(jLabel2, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+				            .addComponent(jLabel1, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+				            .addComponent(lblValue, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+				            .addComponent(btnFirst, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+				            .addComponent(btnAdd, GroupLayout.Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE))
+				        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				        .addGroup(thisLayout.createParallelGroup()
+				            .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+				                .addComponent(cbValue, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)
+				                .addGap(0, 37, Short.MAX_VALUE))
+				            .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+				                .addComponent(cbType, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)
+				                .addGap(0, 37, Short.MAX_VALUE))
+				            .addComponent(txtSearch, GroupLayout.Alignment.LEADING, 0, 138, Short.MAX_VALUE)
+				            .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+				                .addComponent(btnNext, GroupLayout.PREFERRED_SIZE, 75, GroupLayout.PREFERRED_SIZE)
+				                .addGap(0, 63, Short.MAX_VALUE))))
+				    .addComponent(jScrollPane1, GroupLayout.Alignment.LEADING, 0, 223, Short.MAX_VALUE))
+				.addContainerGap());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- trunk/DSexplorer/src/luz/dsexplorer/gui/ProcessTree.java
+++ trunk/DSexplorer/src/luz/dsexplorer/gui/ProcessTree.java
@@ -1,16 +1,10 @@
 package luz.dsexplorer.gui;
 
-import java.lang.reflect.Field;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import javax.swing.JTree;
-import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 
 import luz.dsexplorer.objects.Process;
+import luz.dsexplorer.objects.ResultList;
 
 public class ProcessTree extends JTree {
 	private static final long serialVersionUID = 8889377903469038055L;
@@ -20,112 +14,7 @@ public class ProcessTree extends JTree {
 	}
 	
 	public void setProcess(Process p){
-		this.setModel(new DefaultTreeModel(new OutlineNode(p)));
-	}
-	
-	
-	private final class OutlineNode extends DefaultMutableTreeNode {
-		private static final long serialVersionUID = -6288860782298224755L;
-		private boolean areChildrenDefined = false;
-		private Object node;
-		private String name;
-		@SuppressWarnings("unchecked")
-		private Class clazz;
-		
-		public OutlineNode(Object object) {
-			this.node = object;
-			this.name = "";
-			this.clazz = object==null?Object.class:object.getClass();
-		}
-
-		@SuppressWarnings("unchecked")
-		public OutlineNode(Object objects, Class clazz, String name) {
-			this.node = objects;
-			this.name = name;
-			this.clazz = clazz;
-		}
-		
-		public Object getNode(){
-			return node;
-		}
-		
-		public String getNodeInfo(){
-			if (node==null)
-				return "<null>";
-			
-			String info="<err>";
-			try{
-				info=node.toString();
-			}catch(Throwable e){}			
-			return info;
-		}
-		
-		
-
-		@Override
-		public boolean isLeaf() {
-			if (node == null)				return true;
-			if (node instanceof Character)	return true;
-			if (node instanceof String)	return true;
-			if (node instanceof Number)	return true;
-			if (node instanceof Date)		return true;
-			if (node instanceof Boolean)	return true;
-			return false;
-		}
-
-		@Override
-		public int getChildCount() {
-			if (!areChildrenDefined)
-				defineChildNodes();
-			return (super.getChildCount());
-		}
-
-		@SuppressWarnings({ "unchecked"})
-		private void defineChildNodes() {
-			areChildrenDefined = true;
-			try {
-				if (node instanceof Object[]){
-					for (Object object : (Object[]) node)
-						add(new OutlineNode(object));
-				
-				}else if (node instanceof List){
-					for (Object object : (List) node)
-						add(new OutlineNode(object));
-				
-				}else if (node instanceof Set){
-					for (Object object : (Set) node)
-						add(new OutlineNode(object));
-				
-				}else if (node instanceof Map){
-					for (Object object : ((Map)node).entrySet())
-						add(new OutlineNode(object));
-			
-				} else{
-					addFields(node);
-				} 
-			}catch (Throwable e) {}
-
-		}
-		
-		@SuppressWarnings("unchecked")
-		private void addFields(Object obj){
-			Class clazz = obj.getClass();
-			while (clazz!=Object.class){
-				for (Field field : clazz.getDeclaredFields()) {
-					field.setAccessible(true);
-					try {
-						add(new OutlineNode(field.get(obj), field.getType(), field.getName()));
-					} catch (Throwable e) {}
-				}
-				clazz=clazz.getSuperclass();
-			}
-		}
-
-		@Override
-		public String toString() {
-			if (getParent()==null) return "root";		
-			return "["+clazz.getSimpleName()+"] "+name+"="+getNodeInfo();
-		}
+		this.setModel(new DefaultTreeModel(new ResultList(p)));
 	}
 
 }
