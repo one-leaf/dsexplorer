a5f5e0a08745d028bf144802e7648ebf3aec0dcc
luzifer42 <luzifer42@a247ce00-be0d-11de-ac5a-05e7513655e2>
2009-12-15 09:27:05 +0000

    added memory area overlapping for effective searching.

--- DSexplorer/src/luz/dsexplorer/winapi/api/MemoryListener.java
+++ DSexplorer/src/luz/dsexplorer/winapi/api/MemoryListener.java
@@ -4,10 +4,12 @@ import luz.dsexplorer.objects.datastructure.DSType;
 
 import com.sun.jna.Memory;
 
+
 public abstract class MemoryListener {
 	private ResultList results;
 	private String value;
 	private DSType type;
+	private int overlapping;
 
 	public void add(Result result) {
 		results.add(result);
@@ -21,10 +23,34 @@ public abstract class MemoryListener {
 		return type;
 	}
 	
-	public void init(ResultList results2, String value, DSType type){
-		this.results=results2;
+	/**
+	 * To optimize memory reading, the reading algorithm divides large memory areas into
+	 * smaller ones. This value forces the reading algorithm to overlap these memory areas by
+	 * the given value. This is usefull if the seach pattern is longer than one Byte. In this case, 
+	 * the MemoryListener has only to worry about one area at each time. Values at the end of an 
+	 * area will repeat at the beginning of the next area. In general this value should be 
+	 * 'size of the search pattern -1'.
+	 * @param overlapping
+	 */
+	public int getOverlapping(){
+		return overlapping;
+	}
+		
+	public void init(ResultList results, String value, DSType type){
+		this.results=results;
 		this.value=value;
 		this.type=type;
+		switch (type){
+			case Byte1:		overlapping=0;	break;	
+			case Byte2:		overlapping=1;	break;	
+			case Byte4:		overlapping=3;	break;	
+			case Byte8:		overlapping=7;	break;
+			case Float:		overlapping=3;	break;	
+			case Double:	overlapping=7;	break;
+			case ByteArray:	overlapping=value.length()/2;	break;	
+			case Ascii:		overlapping=value.length();		break;	
+			case Unicode:	overlapping=value.length();		break;	
+		}
 	}
 	
 	public abstract void mem(Memory outputBuffer, long address, long size);
