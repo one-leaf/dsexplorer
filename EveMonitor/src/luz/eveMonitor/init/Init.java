7a47c00486766a1cba68aab5ff641022e1716c55
Luzifer42 <Luzifer42@a247ce00-be0d-11de-ac5a-05e7513655e2>
2010-02-08 20:18:15 +0000

    separated winapi, evemonitor and dsexplorer

---
+++ EveMonitor/src/luz/eveMonitor/init/Init.java
@@ -0,0 +1,72 @@
+package luz.eveMonitor.init;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import luz.eveMonitor.gui.MainWindow;
+import luz.eveMonitor.threads.DictFinder;
+import luz.eveMonitor.threads.ProcessFinder;
+import luz.eveMonitor.threads.RowFinder;
+import luz.eveMonitor.threads.Status;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Init {
+	private static final Log log = LogFactory.getLog(Init.class);
+	private static MainWindow window;
+	private static Status status;
+	private static ProcessFinder pf;
+	private static DictFinder df;
+	private static RowFinder rf;
+	
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			//UIManager.put("Table.font", new Font("Lucida Console", Font.PLAIN, 11));
+		} catch (ClassNotFoundException e)          { log.warn("getSystemLookAndFeelClassName", e);
+		} catch (InstantiationException e)          { log.warn("getSystemLookAndFeelClassName", e);
+		} catch (IllegalAccessException e)          { log.warn("getSystemLookAndFeelClassName", e);
+		} catch (UnsupportedLookAndFeelException e) { log.warn("getSystemLookAndFeelClassName", e);
+		}
+
+		window=new MainWindow();
+		status=new Status();
+		pf = new ProcessFinder(status);
+		df = new DictFinder(status);
+		rf = new RowFinder(status, window);
+		
+		pf.start();
+		df.start();
+		rf.start();
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				window.addWindowListener(new WindowListener() {
+					public void windowOpened(WindowEvent e) {}
+					public void windowIconified(WindowEvent e) {}
+					public void windowDeiconified(WindowEvent e) {}
+					public void windowDeactivated(WindowEvent e) {}
+					public void windowClosing(WindowEvent e) {}
+					public void windowActivated(WindowEvent e) {}
+					
+					@Override
+					public void windowClosed(WindowEvent e) {
+				        pf.stopNow();
+				        df.stopNow();
+				        rf.stopNow();
+					}
+				});
+				window.setLocationRelativeTo(null);
+				window.setVisible(true);
+			}
+		});
+
+	}
+
+
+}
